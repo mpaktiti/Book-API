c44814e05bb88df3f04b7f0dc16fe795e084c7db
mpaktiti <maria.paktiti@gmail.com>
2019-05-23 20:35:31 +0300

    Implement PATCH /books/:bookId

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -38,8 +38,19 @@ function routes(Book) {
     book.author = req.body.author;
     book.genre = req.body.genre;
     book.read = req.body.read;
-    book.save();
-    return res.json(book);
+    req.book.save((err) => {
+      if (err) return res.send(err);
+      return res.json(book);
+    });
+  })
+  .patch((req, res) => {
+    const { book } = req;
+    if (req.body._id) delete req.body._id;
+    Object.entries(req.body).forEach(item => book[item[0]] = item[1]);
+    req.book.save((err) => {
+      if (err) return res.send(err);
+      return res.json(book);
+    });
   });
 
   return bookRouter;
