752aa08dc9e8a8eac48cdce2909169afe5770cd2
mpaktiti <maria.paktiti@gmail.com>
2019-05-28 16:56:32 +0300

    Fix broken eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true,
+    node: true,
+    mocha: true,
   },
   extends: 'airbnb-base',
   rules: {
     'comma-dangle': 0
-  }
+  },
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -14,7 +14,5 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-  },
-  rules: {
-  },
+  }
 };
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -6,8 +6,8 @@ function routes(Book) {
   const controller = booksController(Book);
 
   bookRouter.route('/books')
-  .get(controller.get)
-  .post(controller.post)
+    .get(controller.get)
+    .post(controller.post);
 
   bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
@@ -20,33 +20,33 @@ function routes(Book) {
     });
   });
   bookRouter.route('/books/:bookId')
-  .get((req, res) => res.json(req.book))
-  .put((req, res) => {
-    const { book } = req;
-    book.title = req.body.title;
-    book.author = req.body.author;
-    book.genre = req.body.genre;
-    book.read = req.body.read;
-    req.book.save((err) => {
-      if (err) return res.send(err);
-      return res.json(book);
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
+        if (err) return res.send(err);
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) delete req.body._id;
+      Object.entries(req.body).forEach(item => book[item[0]] = item[1]);
+      req.book.save((err) => {
+        if (err) return res.send(err);
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) return res.send(err);
+        return res.sendStatus(204);
+      });
     });
-  })
-  .patch((req, res) => {
-    const { book } = req;
-    if (req.body._id) delete req.body._id;
-    Object.entries(req.body).forEach(item => book[item[0]] = item[1]);
-    req.book.save((err) => {
-      if (err) return res.send(err);
-      return res.json(book);
-    });
-  })
-  .delete((req, res) => {
-    req.book.remove((err) => {
-      if (err) return res.send(err);
-      return res.sendStatus(204);
-    });
-  });
 
   return bookRouter;
 }
